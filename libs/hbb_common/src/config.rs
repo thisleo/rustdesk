9addbae7f7bf1bbc86e5034b6632293054291175
Chendy <cdyiwhy@yahoo.com>
2024-10-16 15:24:22 +0800

    config.rs
    modify key and domain

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["matlab.eu.org"];
+pub const PUBLIC_RS_PUB_KEY: &str = "tT0SFf6TKZ99H3SrxYYGOWj0IioLe9YLPKXFr6j9SYU=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
